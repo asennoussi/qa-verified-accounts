7fa899c5c6f4c8ff8ccb8fb1a505bcf35351ab8f
Aymane <aymane.sennoussi@gmail.com>
2018-03-31 17:30:39 +0100

    align with the new who logic

--- qa-verified-accounts-layer.php
+++ qa-verified-accounts-layer.php
@@ -5,11 +5,14 @@ class qa_html_theme_layer extends qa_html_theme_base
 
     function post_meta_who($post, $class) // show usernames of privileged users in italics
     {
+        /*TODO: Check $post['Who'] Level*/
         require_once QA_INCLUDE_DIR.'qa-app-users.php'; // for QA_USER_LEVEL_BASIC constant
 
-        if (isset($post['raw']['opostid'])) // if item refers to an answer or comment...
+        /*
+         * if (isset($post['raw']['opostid'])) // if item refers to an answer or comment...
             $level=@$post['raw']['olevel']; // ...take the level of answer or comment author
         else
+         */
             $level=@$post['raw']['level']; // otherwise take level of the question author
 
         if ($level>=QA_USER_LEVEL_EXPERT){  // if level is more than Expert user..
